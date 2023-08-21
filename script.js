0fd1573b3cf3a6abc76e4ecaf43bad99aea64d4c
ClaudiaCristinaTomus <tomus_clau@yahoo.com>
2023-08-21 12:05:14 +0300

    carousel

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+const carousel = document.querySelector(".carousel");
+const arrowBtns = document.querySelectorAll(".wrapper i");
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const carouselChildrens = [...carousel.children];
+
+let isDragging = false, startX, startScrollLeft;
+
+
+
+
+
+arrowBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
   });
+})
+
+const dragStart = (e) =>{
+  isDragging = true;
+  carousel.classList.add("dragging");
+  startX = e.pageX;
+  startScrollLeft= carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+  if (!isDragging) return;
+  carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop =()=>{
+  isDragging =  false;
+  carousel.classList.remove("dragging");
+}
+
+carousel.addEventListener("mousedown",dragStart);
+carousel.addEventListener("mousemove",dragging);
+carousel.addEventListener("mouseup", dragStop);
